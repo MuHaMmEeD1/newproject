6bf14ffc026b004b4c23706d61a8430988fd9990
Muhamməd Nəbiyev <aliyevasamire596@gmail.com>
2025-01-29 07:35:54 +0400

    new_1

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
-import React from 'react'
-import "./global.css"
-import Login from './src/screens/login/Login'
-import Home from './src/screens/home/Home'
+import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import React from 'react';
+import './global.css';
 
 const App = () => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        {/* <Login/> */}
-        <Home />
-      </SafeAreaView>
+      <SafeAreaView style={{flex: 1}}></SafeAreaView>
     </SafeAreaProvider>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
